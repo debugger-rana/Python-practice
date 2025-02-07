b1d7e0fe406d51db618d2fe32fef69e17f3b8300
Rana Pratap Singh Patel <ranapsp317@gmail.com>
2025-02-07 23:45:20 +0530

    __init__ methode

---
+++ opps/__init__Methode.py
@@ -0,0 +1,10 @@
+class name:
+    def __init__(self,name,age,pro):
+        self.name=name
+        self.age=age
+        self.prfession=pro
+
+    def about(self):
+        print("about {self.name}",self.age,self.prfession)
+
+rana=name("")
--- opps/classObject.py
+++ opps/classObject.py
@@ -1,9 +1,16 @@
 class rana:
     def character(self):
-        gender:'male'
+        gender:"male"
         age:23
         print("rana")
 
 r1=rana()
+r2=rana()
+# calling the object's methode with the help of class and passing the object as argument
 rana.character(r1)
-# print()
+rana.character(r2)
+print()
+# calling the object's methode direclty from the oject itself 
+r1.character()
+r2.character()
+    
