8d61c4af1339b753d57f343c5bcea2ac0d93da66
Rana Pratap Singh Patel <ranapsp317@gmail.com>
2025-02-08 16:12:11 +0530

    updates on course

--- opps/__init__Methode.py
+++ opps/__init__Methode.py
@@ -2,9 +2,30 @@ class name:
     def __init__(self,name,age,pro):
         self.name=name
         self.age=age
-        self.prfession=pro
+        self.profession=pro
 
     def about(self):
-        print("about {self.name}",self.age,self.prfession)
+        print(f"about {self.name}",self.age,self.profession)
 
-rana=name("")
+    def ageUpdate(self,other):
+        self.age=other.age
+
+    def compare(self,other):
+        if self.age==other.age:
+            return True
+        else:
+            return False
+
+rana=name("rana",21,"hacker")
+rana.about()
+rohit=name("rohit",22,'account')
+rohit.about()
+
+# rohit.ageUpdate(rana)
+
+if rana.compare(rohit):
+    print("the age are same")
+else:
+    print("Age are different")
+
+
